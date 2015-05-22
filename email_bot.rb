fea762a64910558b24d4498bc6ec1ef2d65f9155
chiu <chiucode@gmail.com>
2015-05-22 19:41:42 +0000

    great

---
+++ email_bot.rb
@@ -0,0 +1,17 @@
+# email_bot.rb
+require 'mail'
+
+mail = Mail.new do
+  from    'mikel@test.lindsaar.net'
+  to      'you@test.lindsaar.net'
+  subject 'This is a test email'
+  body    File.read('body.txt')
+end
+
+
+
+
+mail.to_s #=> "From: mikel@test.lindsaar.net\r\nTo: you@...
+
+
+puts "hi"
