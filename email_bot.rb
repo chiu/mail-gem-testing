bf07283e31a26b727dc858f94a544096a95fcc8a
chiu <chiucode@gmail.com>
2015-05-22 19:55:57 +0000

    almost working

--- email_bot.rb
+++ email_bot.rb
@@ -1,17 +1,46 @@
 # email_bot.rb
 require 'mail'
 
-mail = Mail.new do
-  from    'mikel@test.lindsaar.net'
-  to      'you@test.lindsaar.net'
-  subject 'This is a test email'
-  body    File.read('body.txt')
+# mail = Mail.new do
+#   from    'mikel@test.lindsaar.net'
+#   to      'you@test.lindsaar.net'
+#   subject 'This is a test email'
+#   body    File.read('body.txt')
+# end
+
+
+
+
+# mail.to_s #=> "From: mikel@test.lindsaar.net\r\nTo: you@...
+
+
+# puts "hi"
+# puts mail.to_s
+Mail.defaults do
+  delivery_method :smtp, address: "localhost", port: 1025
+end
+
+# Sending via GMail
+
+Mail.defaults do
+  delivery_method :smtp, { :address              => "smtp.gmail.com",
+                           :port                 => 587,
+                           :domain               => 'your.host.name',
+                           :user_name            => '<username>',
+                           :password             => '<password>',
+                           :authentication       => 'plain',
+                           :enable_starttls_auto => true  }
 end
 
 
+mail = Mail.new do
+  from     'me@test.lindsaar.net'
+  to       'you@test.lindsaar.net'
+  subject  'Here is the image you wanted'
+  body     File.read('body.txt')
+  # add_file :filename => 'somefile.png', :content => File.read('./somefile.png')
+end
 
+# mail.delivery_method :sendmail
 
-mail.to_s #=> "From: mikel@test.lindsaar.net\r\nTo: you@...
-
-
-puts "hi"
+mail.deliver
